79807c2512d4240a3e779ad288891069cbe61995
zarifatb-15 <zarifatb@code.edu.az>
2025-11-19 01:20:23 +0400

    first commit

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Practice;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Arrayde en boyuk reqemin indeksini tapir.Axtardigimiz reqem yoxusa ekrana -1 cixarir.
+      
+        // int[] numbers = { 10, 20, 15 };
+        // Array.Sort(numbers);
+        // int num = 10;
+        // int index = -1;
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     if (numbers[i] == num)
+        //     {
+        //         index = i;
+        //         break;
+        //     }
+        // }
+        // Console.WriteLine($"Index: {index}");
+//Arraydə təkrarlanan rəqəmi tpamaq və neçə dəfə təkrarlandığını ekrana çap etmek.
+        // int[] arr = { 1, 2, 3, 4, 5, 5, 5, 4, 3, 3 };
+        // int num = 5;
+        // int count = 0;
+        // for (int i = 0; i < arr.Length; i++)
+        // {
+        //     if (arr[i] == num)
+        //         count++;
+        // }
+        //
+        // Console.WriteLine($"Count:{count}");
+        // Tekrarlanan simvol ve sayi
+
+        // string sentence = "Hello World helloooo";
+        // int max = 0;
+        // char simvol = ' ';
+        // for (int i = 0; i < sentence.Length; i++)
+        // {
+        //     int count = 0;
+        //     for (int j = 0; j < sentence.Length; j++)
+        //     {
+        //         if (sentence[i] == sentence[j])
+        //             count++;
+        //         if (count > max)
+        //         {
+        //             max = count;
+        //             simvol = sentence[i];
+        //         }
+        //     }
+        // }
+        //
+        // Console.WriteLine($"Simvol: {simvol}");
+        // Console.WriteLine($"MAX Simvol: {max}");
+    }
+}
