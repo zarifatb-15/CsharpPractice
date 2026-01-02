6acbc4da15e43281cbab63fdf2a806cfad945ef0
zarifatb-15 <zarifatb@code.edu.az>
2026-01-03 00:03:15 +0400

    Added taks

--- Practice/Practice/Program.cs
+++ Practice/Practice/Program.cs
@@ -6,7 +6,56 @@ class Program
 {
     static void Main(string[] args)
     {
+        #region  ArrayTasks
+
         //Arrayde en boyuk reqemin indeksini tapir.Axtardigimiz reqem yoxusa ekrana -1 cixarir.
+      
+        // int[] numbers = { 10, 20, 15 };
+        // Array.Sort(numbers);
+        // int num = 10;
+        // int index = -1;
+        // for (int i = 0; i < numbers.Length; i++)
+        // {
+        //     if (numbers[i] == num)
+        //     {
+        //         index = i;
+        //         break;
+        //     }
+        // }
+        // Console.WriteLine($"Index: {index}");
+//Arraydə təkrarlanan rəqəmi tpamaq və neçə dəfə təkrarlandığını ekrana çap etmek.
+        // int[] arr = { 1, 2, 3, 4, 5, 5, 5, 4, 3, 3 };
+        // int num = 5;
+        // int count = 0;
+        // for (int i = 0; i < arr.Length; i++)
+        // {
+        //     if (arr[i] == num)
+        //         count++;
+        // }
+        //
+        // Console.WriteLine($"Count:{count}");
+        // Tekrarlanan simvol ve sayi
+
+        // string sentence = "Hello World helloooo";
+        // int max = 0;
+        // char simvol = ' ';
+        // for (int i = 0; i < sentence.Length; i++)
+        // {
+        //     int count = 0;
+        //     for (int j = 0; j < sentence.Length; j++)
+        //     {
+        //         if (sentence[i] == sentence[j])
+        //             count++;
+        //         if (count > max)
+        //         {
+        //             max = count;
+        //             simvol = sentence[i];
+        //         }
+        //     }
+        // }
+        //
+        // Console.WriteLine($"Simvol: {simvol}");
+        // Console.WriteLine($"MAX Simvol: {max}");//Arrayde en boyuk reqemin indeksini tapir.Axtardigimiz reqem yoxusa ekrana -1 cixarir.
       
         // int[] numbers = { 10, 20, 15 };
         // Array.Sort(numbers);
@@ -55,8 +104,67 @@ class Program
         // Console.WriteLine($"Simvol: {simvol}");
         // Console.WriteLine($"MAX Simvol: {max}");
         
+        // string[] students=new string[3];
+        //
+        // students[0] = "Zarifa";
+        // students [1] = "Kamran";
+        // students [2] = "Aysel";
+        //
+        // foreach (string student in students)
+        // {
+        // Console.WriteLine(student);
+        // }
+            
+        
+
+        // string [] studentGroup = new string [5];
+        // studentGroup[0] = "Ayxan";
+        // studentGroup[1] = "Rufet";
+        // studentGroup[2] = "Nicat";
+        // studentGroup[3] = "Xeyale";
+        // studentGroup[4] = "Zarina";
+        //
+        //
+        // Console.WriteLine(studentGroup[0]);
+        
+        // foreach (string student in studentGroup)
+        // {
+        //     Console.WriteLine(student);
+        // }
+
+        // string[,] regions = new string[7, 3]
+        // {
+        //     { "Baki", "Gence", "Sumqayit" },
+        //     { "Lankaran", "Mingachevir", "Shaki" },
+        //     { "Quba", "Qusar", "Shamakhi" },
+        //     { "Gabala", "Yevlakh", "Agdam" },
+        //     { "Nakhchivan", "Shirvan", "Jalilabad" },
+        //     { "Salyan", "Masalli", "Fuzuli" },
+        //     { "Imishli", "Aghjabadi", "Tartar" }
+        // };
+        //
+        // for (int i=0;i<=regions.GetUpperBound(0); i++)
+        // {
+        //     for (int j = 0; j <= regions.GetUpperBound(1); j++)
+        //     {
+        //         Console.WriteLine(regions[i, j]);
+        //     }
+        // }
+        //
         
         
+        
+
+
+
+
+
+
+        #endregion
+        #region MyMiniTasks
+
+        // Console.WriteLine("*****Hello C#*****");
+
         // Console.WriteLine("*****Qiymet Cedveli*****");
         // Console.WriteLine();
         // double applePrice = 2.50;
@@ -136,10 +244,72 @@ class Program
         // Console.WriteLine("Passenger Age:" + passengerAge);
         // Console.WriteLine("Passenger Identity:" + passengerIdentity);
         //
+
+
+        // int shoesPrice, computerPrice, chairPrice, tvPrice;
+        // shoesPrice = 100;
+        // computerPrice = 3300;
+        // chairPrice = 500;
+        // tvPrice= 2500;
+        //
+        // int shoesCount, computerCount, chairCount, tvCount;
+        // Console.Write(" Aldiginiz ayaqqabi sayini yazin: ");
+        // shoesCount = int.Parse(Console.ReadLine());
+        // Console.Write("Aldiginiz komputer sayini yazin: ");
+        // computerCount=int.Parse(Console.ReadLine());
+        // Console.Write("Aldiginiz stul sayini yazin: ");
+        // chairCount = int.Parse(Console.ReadLine());
+        // Console.Write("Aldiginiz televizor sayini yazin: ");
+        // tvCount = int.Parse(Console.ReadLine());
+        //
+        // int totalPrice=(shoesPrice*shoesCount)+(computerPrice*computerCount)+(chairPrice*chairCount)+(tvPrice*tvCount);
+        // Console.WriteLine();
+        //
+        // Console.WriteLine("Umumi mebleg: " + totalPrice + " AZN");
+
+        //5 imtahan balinin ortalamasini tapan proqram
+
+        //  double exam1, exam2, exam3,exam4, exam5;
+        //  Console.Write("Birinci imtahan balini daxil edin: ");
+        //  exam1 = double.Parse(Console.ReadLine());
+        //  Console.Write("Ikinci imtahan balini daxil edin: ");
+        //  exam2= double.Parse(Console.ReadLine());
+        //  Console.Write("Ucuncu imtahan balini daxil edin: ");
+        //  exam3 = double.Parse(Console.ReadLine());
+        //  Console.Write("Dorduncu imtahan balini daxil edin: ");
+        //  exam4 = double.Parse(Console.ReadLine());
+        //  Console.Write("Besinci imtahan balini daxil edin: ");
+        //  exam5 = double.Parse(Console.ReadLine());
+        //  
+        // double result  = (exam1 + exam2 + exam3+ exam4+ exam5) / 5;
+        //  Console.WriteLine("Ortalama baliniz: " + result);
+
+        #endregion
+
+        #region Lopps
+
+        // var i1 = For();
+        //
+        //
+        // for(int i =100; i<0; i--)
+        // {
+        //     Console.WriteLine(i);
+        // }
         
         
         
 
-
+        #endregion
     }
+
+    // private static int For()
+    // {
+    //     int i;
+    //     for (i = 0; i <=100; i++)
+    //     {
+    //         Console .WriteLine(i);
+    //     }
+    //
+    //     return i;
+    // }
 }
